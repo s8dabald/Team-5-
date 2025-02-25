4dbf9ddc98bca301ff1fa565fb23118fa06a9c1d
Fatima87388 <ambreenf809@gmail.com>
2025-02-25 18:39:07 +0100

    Removed Print Commands

--- employee_anlaysis.py
+++ employee_anlaysis.py
@@ -6,12 +6,10 @@ def get_employee_data():#to get data from DB
     employee_data = execute_db_query("SELECT * FROM Employee_DB", as_dict=True)
     #after the db execute puts  it in a dataframe if the data is empty creates  an empty dataframe
     employees = pd.DataFrame([dict(row) for row in employee_data]) if employee_data else pd.DataFrame()
-    print(employees.head())
     return employees
 
 def get_employee_distributions():#gets the different segments
     employees = get_employee_data()
     employee_country_distrbution = employees['Country'].value_counts().to_dict()
     employee_job_distribution = employees['JobId'].value_counts().to_dict()
-    print(employee_job_distribution)
     return employee_country_distrbution, employee_job_distribution
