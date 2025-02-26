78d56c44b6abdf02e8f1494485ac3b4ffe6bde2c
Daniel <h1718746@nwytg.com>
2025-02-26 22:11:50 +0100

    moved most files to a directory

--- services/employee_analysis.py
+++ services/employee_analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 from database_manager import execute_db_query
 
 def get_employee_data():#to get data from DB
+    """"""
     employee_data = execute_db_query("SELECT * FROM Employee_DB", as_dict=True)
     #after the db execute puts  it in a dataframe if the data is empty creates  an empty dataframe
     employees = pd.DataFrame([dict(row) for row in employee_data]) if employee_data else pd.DataFrame()
